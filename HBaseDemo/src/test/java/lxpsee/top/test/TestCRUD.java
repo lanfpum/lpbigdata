3c005246cad1f6048b9e9cebee37d140ed29289e
LANPENG <513421121@qq.com>
2018-09-25 15:53:38 +0800

    [INSERT]添加了hbase day02 代码

--- HBaseDemo/src/test/java/lxpsee/top/test/TestCRUD.java
+++ HBaseDemo/src/test/java/lxpsee/top/test/TestCRUD.java
@@ -9,7 +9,9 @@ import org.junit.Test;
 
 import java.io.IOException;
 import java.text.DecimalFormat;
-
+import java.util.Iterator;
+import java.util.Map;
+import java.util.NavigableMap;
 
 /**
  * The world always makes way for the dreamer
@@ -82,9 +84,19 @@ public class TestCRUD {
         Configuration configuration = HBaseConfiguration.create();
         Connection connection = ConnectionFactory.createConnection(configuration);
         Admin admin = connection.getAdmin();
-        NamespaceDescriptor namespaceDescriptor = NamespaceDescriptor.create("ns2").build();
+        NamespaceDescriptor namespaceDescriptor = NamespaceDescriptor.create("ns1").build();
         admin.createNamespace(namespaceDescriptor);
         System.out.println("名称空间已经创建ok!");
+    }
+
+
+    @Test
+    public void deleteNameSpace() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Admin admin = connection.getAdmin();
+        admin.deleteNamespace("ns2");
+        System.out.println("over");
     }
 
     @Test
@@ -97,7 +109,6 @@ public class TestCRUD {
         for (NamespaceDescriptor namespaceDescriptor : namespaceDescriptors) {
             System.out.println(namespaceDescriptor.getName());
         }
-
     }
 
     @Test
@@ -105,13 +116,114 @@ public class TestCRUD {
         Configuration configuration = HBaseConfiguration.create();
         Connection connection = ConnectionFactory.createConnection(configuration);
         Admin admin = connection.getAdmin();
-        TableName tableName = TableName.valueOf("ns2:t1");
+        TableName tableName = TableName.valueOf("ns1:t1");
         HTableDescriptor hTableDescriptor = new HTableDescriptor(tableName);
         hTableDescriptor.addFamily(new HColumnDescriptor("f1"));
         admin.createTable(hTableDescriptor);
         System.out.println("over");
     }
 
+    @Test
+    public void disableTable() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Admin admin = connection.getAdmin();
+        admin.disableTable(TableName.valueOf("ns2:t1"));
+//        admin.enableTable(TableName.valueOf("ns1:t1"));
+        admin.deleteTable(TableName.valueOf("ns2:t1"));
+        System.out.println("over");
+    }
+
+    @Test
+    public void deleteData() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf("ns1:t1"));
+        Delete delete = new Delete(Bytes.toBytes("row00001"));
+//        delete.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("id"));
+//        delete.addColumn(Bytes.toBytes("f1"), Bytes.toBytes("age"));
+        table.delete(delete);
+        System.out.println("over");
+    }
+
+    @Test
+    public void scan() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf("ns1:t1"));
+        Scan scan = new Scan();
+        scan.setStartRow(Bytes.toBytes("row02000"));
+        scan.setStopRow(Bytes.toBytes("row05000"));
+        ResultScanner resultScanner = table.getScanner(scan);
+        Iterator<Result> iterator = resultScanner.iterator();
+
+        while (iterator.hasNext()) {
+            Result result = iterator.next();
+            byte[] value = result.getValue(Bytes.toBytes("f1"), Bytes.toBytes("name"));
+            System.out.println(Bytes.toString(value));
+        }
+    }
+
+    @Test
+    public void scan2() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf("ns1:t1"));
+        Scan scan = new Scan();
+        scan.setStartRow(Bytes.toBytes("row03000"));
+        scan.setStopRow(Bytes.toBytes("row04000"));
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> iterator = scanner.iterator();
+
+        while (iterator.hasNext()) {
+            Result next = iterator.next();
+            Map<byte[], byte[]> f1Map = next.getFamilyMap(Bytes.toBytes("f1"));
+
+            for (Map.Entry<byte[], byte[]> entry : f1Map.entrySet()) {
+                String col = Bytes.toString(entry.getKey());
+                String val = Bytes.toString(entry.getValue());
+                System.out.print(col + " : " + val + ";");
+            }
+
+            System.out.println();
+        }
+    }
+
+    @Test
+    public void scan3() throws IOException {
+        Configuration configuration = HBaseConfiguration.create();
+        Connection connection = ConnectionFactory.createConnection(configuration);
+        Table table = connection.getTable(TableName.valueOf("ns1:t1"));
+        Scan scan = new Scan();
+        scan.setStartRow(Bytes.toBytes("row04000"));
+        scan.setStopRow(Bytes.toBytes("row05000"));
+        ResultScanner scanner = table.getScanner(scan);
+        Iterator<Result> iterator = scanner.iterator();
+
+        while (iterator.hasNext()) {
+            Result next = iterator.next();
+            //得到一行的所有map,key=f1,value=Map<Col,Map<Timestamp,value>>
+            Map<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> rowMap = next.getMap();
+
+            for (Map.Entry<byte[], NavigableMap<byte[], NavigableMap<Long, byte[]>>> rowMapEntry : rowMap.entrySet()) {
+                String f = Bytes.toString(rowMapEntry.getKey());
+                Map<byte[], NavigableMap<Long, byte[]>> colData = rowMapEntry.getValue();
+
+                for (Map.Entry<byte[], NavigableMap<Long, byte[]>> colDataEntry : colData.entrySet()) {
+                    String col = Bytes.toString(colDataEntry.getKey());
+                    Map<Long, byte[]> tsData = colDataEntry.getValue();
+
+                    for (Map.Entry<Long, byte[]> longEntry : tsData.entrySet()) {
+                        Long key = longEntry.getKey();
+                        String val = Bytes.toString(longEntry.getValue());
+                        System.out.print(f + " - " + col + " - " + key + " : " + val + ";");
+                    }
+                }
+            }
+
+            System.out.println();
+        }
+    }
 
     @Test
     public void formatNum() {
